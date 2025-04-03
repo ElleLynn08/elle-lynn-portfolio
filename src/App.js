7e3d82f5e2695c283cd435bf8d051b81ea254ded
ElleLynn08 <m.lynn8@me.com>
2025-04-03 03:24:48 -0400

    🚀 Final build & deployment-ready

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
-import About from "./pages/About";
+import About from "./pages/AboutPage";
 import Work from "./pages/Work";
 import Contact from "./pages/Contact"; 
 
