a8c340a86b4917df5374fc616097033978d1135e
ElleLynn08 <m.lynn8@me.com>
2025-10-02 19:35:02 -0400

    updated contact page, a little glow up

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -5,15 +5,17 @@ function Contact() {
   return (
     <div className="contact-page container">
       <p className="contact-intro">
-        Whether you’re building something meaningful, solving complex problems, or simply seeking a teammate who brings both technical clarity and human empathy—I’d love to connect. Let’s make something impactful together.
+        Whether you’re building something meaningful, tackling complex challenges, or seeking a collaborator who blends technical clarity with deep empathy—I’d love to connect.
+        Let’s build something ethical. Something intuitive. Something that matters.
       </p>
 
       <p className="signature-typewriter">
-         Let’s build something real.
+        <em>Let’s make something impactful together.</em>
       </p>
     </div>
   );
 }
 
 export default Contact;
+
 
